f3120140dd72435609ee6a4836c2afe4f5e13de5
ljy2855 <ljy2855@naver.com>
2022-10-07 20:30:04 +0900

    fix: Change server url

--- lib/url.dart
+++ lib/url.dart
@@ -1,6 +1,6 @@
 class UrlPrefix {
   //static var urls = "https://api.cspc.me/";
-  static var urls = "http://10.0.2.2/";
-  // static var urls =
-  // "http://ec2-13-209-88-53.ap-northeast-2.compute.amazonaws.com/";
+  // static var urls = "http://10.0.2.2/";
+  static var urls =
+      "http://ec2-13-209-88-53.ap-northeast-2.compute.amazonaws.com/";
 }
